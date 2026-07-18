acadbff1a2d4ebc29aa8e9334570ff8b2c0e4d96
rishabh1244 <1244rishabh@gmail.com>
2026-07-18 22:59:02 +0530

    storage optimisation

--- src/orderbook/route.rs
+++ src/orderbook/route.rs
@@ -19,7 +19,7 @@ static ORDER_BOOK: OrderBook = OrderBook {
     asks: Mutex::new(Vec::new()),
 };
 
-async fn searchMatch(order: &Order, order_type: &str) -> i32 {
+fn searchMatch(order: &Order, order_type: &str) -> i32 {
     let opposite_book = if order_type == "bid" {
         &ORDER_BOOK.asks
     } else {
@@ -28,7 +28,32 @@ async fn searchMatch(order: &Order, order_type: &str) -> i32 {
 
     let mut book = opposite_book.lock().unwrap();
     let mut remaining = order.qty as i64;
-    let mut i = 0;
+
+    if order_type == "ask"{
+        let mut i = len(&ORDER_BOOK);
+        while i > 0 {
+            if remaining <= 0{
+                break ;
+            }
+
+            let price_match = if book[i].price <= order.price; 
+            if !price_match {
+            i += 1;
+            continue;
+        }
+
+        let match_qty = book[i].qty.min(remaining as u64);
+        remaining -= match_qty as i64;
+        book[i].qty -= match_qty;
+
+        if book[i].qty == 0 {
+            book.swap_remove(i);
+        } else {
+            i += 1;
+        }
+        }
+    }
+
 
     while i < book.len() {
         if remaining <= 0 {
@@ -78,27 +103,50 @@ impl OrderBook {
         println!("----------------------");
     }
 }
+/*
+curl -X POST 'http://127.0.0.1:8081/placeOrder?order_type=ask' \
+  -H 'Content-Type: application/json' \
+  -d '{"userId":"user123","qty":10,"price":100}'&&
+curl -X POST 'http://127.0.0.1:8081/placeOrder?order_type=ask' \
+  -H 'Content-Type: application/json' \
+  -d '{"userId":"user123","qty":10,"price":101}'
+curl -X POST 'http://127.0.0.1:8081/placeOrder?order_type=ask' \
+  -H 'Content-Type: application/json' \
+  -d '{"userId":"user123","qty":10,"price":102}'
+
+*/
+
 async fn order_book_append(order_type: String, order: Order) {
     let book = if order_type == "bid" {
         &ORDER_BOOK.bids
     } else {
         &ORDER_BOOK.asks
     };
-    book.lock().unwrap().push(order);
+    let mut book = book.lock().unwrap();
+    let pos = if order_type == "bid" {
+        //increasing order
+        book.iter().position(|o| o.price <= order.price)
+    } else {
+        // decreasing order
+        book.iter().position(|o| o.price >= order.price)
+    };
+    match pos {
+        Some(p) => book.insert(p, order),
+        None => book.push(order),
+    }
 }
 
 #[derive(Deserialize)]
 struct PlaceOrderQuery {
     order_type: String,
 }
-
 #[post("/placeOrder")]
 pub async fn placeOrder(
     order: web::Json<Order>,
     query: web::Query<PlaceOrderQuery>,
 ) -> impl Responder {
     let mut order = order.into_inner();
-    let remaining = searchMatch(&order, &query.order_type).await;
+    let remaining = searchMatch(&order, &query.order_type);
 
     if remaining > 0 {
         order.qty = remaining as u64;
