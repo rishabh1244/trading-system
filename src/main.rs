966053b3ed28c8b370b2e62f6ae3464ff2a21d23
rishabh1244 <1244rishabh@gmail.com>
2026-07-27 17:05:26 +0530

    udpate

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,9 @@
-use actix_web::{web, App, HttpServer};
-
-mod db;
+mod api_gateway;
 mod orderbook;
+
+use api_gateway::api;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let database_url = "postgres://localhost/trading_engine";
-    let pool = db::init_pool(database_url)
-        .await
-        .expect("Failed to create database pool");
-
-    println!("Trading engine running on http://127.0.0.1:8081");
-    HttpServer::new(move || {
-        App::new()
-            .app_data(web::Data::new(pool.clone()))
-            .service(orderbook::route::hello)
-            .service(orderbook::route::placeOrder)
-    })
-    .bind(("127.0.0.1", 8081))?
-    .run()
-    .await
+    api::api_gateway().await
 }
