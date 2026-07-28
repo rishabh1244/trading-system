f07b42722b9f1ce908391910428eb5fcf41f3c53
rishabh1244 <1244rishabh@gmail.com>
2026-07-28 15:26:14 +0530

    order route

---
+++ src/OMS/order_management.rs
@@ -0,0 +1,31 @@
+use actix_web::{HttpResponse, HttpMessage, post, web, HttpRequest};
+use serde::Deserialize;
+use sqlx::PgPool;
+
+use crate::auth::common::AuthData;
+use crate::middleware::auth_middleware::Claims;
+
+#[derive(Deserialize)]
+pub struct OrderData {
+    side: String,
+    qty: i32,
+    price: i32,
+}
+#[derive(Deserialize)]
+pub struct OrderDataStore {
+    user_id: i32,
+    side: String,
+    qty: i32,
+    price: i32,
+}
+#[post("/api/order")]
+pub async fn fetch_order(
+    req: HttpRequest,
+    _pool: web::Data<Option<PgPool>>,
+    _req_body: web::Json<AuthData>,
+) -> HttpResponse {
+    let exts = req.extensions();
+    let claims = exts.get::<Claims>().unwrap();
+    println!("username :{}", claims.username);
+    HttpResponse::Ok().finish()
+}
