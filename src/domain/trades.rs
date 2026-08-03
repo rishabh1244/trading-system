7031da184ff8b60298a901702645a896cbf91b12
rishabh1244 <1244rishabh@gmail.com>
2026-08-03 13:57:00 +0530

    trade list struct

--- src/domain/trades.rs
+++ src/domain/trades.rs
@@ -3,11 +3,16 @@ use jsonwebtoken::{EncodingKey, Header, encode};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
-pub struct Trades {
+pub struct Trade {
     pub trade_id: i32,
     pub trader_id: i32,
     pub seller_id: i32,
 
     pub qty: f32,
     pub price: f32,
+}
+
+#[derive(Deserialize)]
+pub struct TradeList {
+    pub trades: Vec<Trade>,
 }
