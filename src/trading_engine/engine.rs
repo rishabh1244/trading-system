cf4a6ff1bd3221bf69fff3f18c3f7c771e1af3d9
rishabh1244 <1244rishabh@gmail.com>
2026-08-04 11:59:23 +0530

    notes and cleanup

--- src/trading_engine/engine.rs
+++ src/trading_engine/engine.rs
@@ -1,11 +1,6 @@
-use serde::{Deserialize, Serialize};
+use crate::domain::common::Balances;
+use crate::domain::trades::TradeList;
 use sqlx::PgPool;
-use std::cmp;
-use std::sync::Mutex;
-use std::task::Wake;
-
-use domain::common::Balances;
-use domain::trades::TradeList;
 
 pub async fn update_balance(
     user_id: i32,
@@ -18,18 +13,18 @@ pub async fn update_balance(
     .bind(user_id)
     .fetch_one(pool)
     .await?;
-
     for trade in trade.trades.iter() {
         if trade.trader_id == user_id {
             // user is the buyer: receives BTC, pays INR
             user_balance.balance_btc += trade.qty as f64;
-            user_balance.balance_inr -= (trade.qty * trade.price) as f64;
+            // total money user is trying to buy for = balance_lock
         } else if trade.seller_id == user_id {
             // user is the seller: gives up BTC, receives INR
             user_balance.balance_btc -= trade.qty as f64;
-            user_balance.balance_inr += (trade.qty * trade.price) as f64;
         }
     }
+
+    // the user's asset will be store in the orderbook in the form of bids / ask's
 
     sqlx::query("UPDATE balances SET balance_btc=$1, balance_inr=$2 WHERE user_id=$3")
         .bind(user_balance.balance_btc)
@@ -40,3 +35,9 @@ pub async fn update_balance(
 
     Ok(user_balance)
 }
+
+// 2 things remaining
+//  - update the balances of user's who have sold there assets (currently only updating the user who
+//  have sent the request for buy/sell)
+//  - update the remaining balance of the user after all the possible trades are fullfiled
+//  - presist the orderbook in the database
