e5eae432ed9ed15807dd54a885333ecb181f29e0
rishabh1244 <1244rishabh@gmail.com>
2026-08-18 19:55:25 +0530

    db fields updated

--- migrations/02_create_balances.sql
+++ migrations/02_create_balances.sql
@@ -1,5 +1,9 @@
 CREATE TABLE balances (
     user_id INTEGER REFERENCES users(id),
+ 
     balance_btc NUMERIC NOT NULL DEFAULT 0,
-    balance_inr NUMERIC NOT NULL DEFAULT 0
+    balance_inr NUMERIC NOT NULL DEFAULT 0,
+ 
+    reserved_btc NUMERIC NOT NULL DEFAULT 0,
+    reserved_btc NUMERIC NOT NULL DEFAULT 0
 );
